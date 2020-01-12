b4f70a0a6605f13ddaa35553fc62778ecb32e21c
aakash14goplani <goplaniaakash14@gmail.com>
2020-01-13 01:37:44 +0530

    initial commit

---
+++ Data_Structures/src/core/data/structures/Stack.java
@@ -0,0 +1,27 @@
+package core.data.structures;
+
+public class Stack<X> {
+	
+	private X item;
+
+	public Stack() {
+		super();
+	}
+	
+	public void push(X element) {
+		
+	}
+	
+	public X pop() {
+		return item;
+	}
+	
+	public boolean empty() {
+		return true;
+	}
+	
+	public X peek() {
+		return item;
+	}
+
+}
